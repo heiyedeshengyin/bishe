d7a2ad9e2277734415059e1a4779d024c0c59594
heiyedeshengyin <270193571@qq.com>
2021-04-06 15:39:38 +0800

    create BisheApplication.java

---
+++ src/main/java/com/hjr/BisheApplication.java
@@ -0,0 +1,11 @@
+package com.hjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BisheApplication.class, args);
+    }
+}
