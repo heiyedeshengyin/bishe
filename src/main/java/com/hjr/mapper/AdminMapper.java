0c095523cd5aa297b89d1e0e414799e297465724
heiyedeshengyin <270193571@qq.com>
2021-04-21 20:32:16 +0800

    adjust mapper method updateStudent & updateAdmin

--- src/main/java/com/hjr/mapper/AdminMapper.java
+++ src/main/java/com/hjr/mapper/AdminMapper.java
@@ -16,6 +16,12 @@ public interface AdminMapper {
     @Select("select * from admin where admin_login_name = #{loginName}")
     Admin findAdminByLoginName(String loginName);
 
-    @Update("update admin set admin_name = #{adminName}, admin_phone = #{adminPhone}, admin_wechat = #{adminWechat}, admin_qq = #{adminQQ}, admin_birthday = #{adminBirthday}, admin_gender = #{adminGender} where admin_id = #{adminId}")
+    @Update("update admin set admin_name = #{adminName}, " +
+            "admin_phone = #{adminPhone}, " +
+            "admin_wechat = #{adminWechat}, " +
+            "admin_qq = #{adminQQ}, " +
+            "admin_birthday = #{adminBirthday}, " +
+            "admin_gender = #{adminGender} " +
+            "where admin_id = #{adminId}")
     void updateAdmin(Admin admin);
 }
--- src/main/java/com/hjr/mapper/StudentMapper.java
+++ src/main/java/com/hjr/mapper/StudentMapper.java
@@ -19,6 +19,14 @@ public interface StudentMapper {
     @Select("select * from student where student_login_name = #{loginName}")
     Student findStudentByLoginName(String loginName);
 
-    @Update("update student set student_name = #{studentName}, student_phone = #{studentPhone}, student_wechat = #{studentWechat}, student_qq = #{studentQQ}, student_birthday = #{studentBirthday}, student_height = #{studentHeight}, student_weight = #{studentWeight}, student_gender = #{studentGender} where student_id = #{studentId}")
+    @Update("update student set student_name = #{studentName}, " +
+            "student_phone = #{studentPhone}, " +
+            "student_wechat = #{studentWechat}, " +
+            "student_qq = #{studentQQ}, " +
+            "student_birthday = #{studentBirthday}, " +
+            "student_height = #{studentHeight}, " +
+            "student_weight = #{studentWeight}, " +
+            "student_gender = #{studentGender} " +
+            "where student_id = #{studentId}")
     void updateStudent(Student student);
 }
